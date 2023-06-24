d7a2ff91141c1fb09f2a9750368e0d8979a24b1a
Pravin Bhadane <pravinbhadane60@gmail.com>
2023-06-24 14:28:51 +0530

    Add project files.

---
+++ Employee_Salary_Tax/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Salary_Tax
+{
+    public class Employee
+    {
+        private static int totalEmployeeCount = 0; 
+        private const double PT = 200; 
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+            
+           Id = ++totalEmployeeCount;
+        }
+
+        public double GetGrossSalary()
+        {
+            return Salary - PT;
+        }
+
+        public static void DisplayEmployeeCount()
+        {
+            Console.WriteLine($"Total Employee Count: {totalEmployeeCount}");
+        }
+    }
+
+   
+}
---
+++ Employee_Salary_Tax/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Employee_Salary_Tax;
+
+class Program
+{
+    static void Main()
+    {
+        Employee emp1 = new Employee { Name = "Pravin", Salary = 50000 };
+        Employee emp2 = new Employee { Name = "Pawan", Salary = 60000 };
+        Employee emp3 = new Employee { Name = "Kartik", Salary = 55000 };
+
+        double emp1GrossSalary = emp1.GetGrossSalary();
+        double emp2GrossSalary = emp2.GetGrossSalary();
+        double emp3GrossSalary = emp3.GetGrossSalary();
+
+        Console.WriteLine($"Employee 1 Gross Salary: {emp1GrossSalary}");
+        Console.WriteLine($"Employee 2 Gross Salary: {emp2GrossSalary}");
+        Console.WriteLine($"Employee 3 Gross Salary: {emp3GrossSalary}");
+
+        Employee.DisplayEmployeeCount();
+    }
+}
